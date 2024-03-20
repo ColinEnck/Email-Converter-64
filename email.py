697aa3d10d9eb6e4cabb5f40853ac22b1756fa15
ColinEnck <colinenck@gmail.com>
2024-03-20 19:43:37 -0400

    finsihed email project!

---
+++ email.py
@@ -0,0 +1,38 @@
+# first command-line argument should be the mail user
+# to decode the attachment, the subject should end with 
+# 'b64' followed by a '-' and the filetype, such as:
+# "b64-csv"
+import sys
+import base64
+
+mailfile = open(f"/var/mail/{sys.argv[1]}")
+filetypes = []
+attachments = []
+skip_lines = 0
+b64_line = False
+for line in mailfile.readlines():
+    if skip_lines > 0: 
+        skip_lines -= 1
+        continue
+    if b64_line:
+        line = line.removesuffix('\n')
+        attachments.append(line)
+        b64_line = False
+        continue
+    words = line.split()
+    if len(words) > 1:
+        if words[0] == "Subject:":
+            for word in words:
+                if "b64" in word:
+                    filetypes.append(word.split("-")[1])
+        elif "base64" in words:
+            skip_lines = 2
+            b64_line = True
+filenames = []
+for i in range(0, len(attachments)):
+    filenames.append(f"attachment{i}.{filetypes[i]}")
+for i in range(0, len(filenames)):
+    with open(filenames[i], "wb") as file:
+        file.write(base64.b64decode(attachments[i]))
+with open(f"/var/mail/{sys.argv[1]}", "w") as file:
+    file.write("")
